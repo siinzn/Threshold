7c480d95033865d0a83678154606000ca7d41a36
siinzn <msinannoufal@gmail.com>
2026-07-21 23:29:23 +0400

    Frozen Lake Example completed - run method added

--- example/frozen_lake.py
+++ example/frozen_lake.py
@@ -13,7 +13,7 @@ sns.set_theme()
 class Params(NamedTuple):
     total_episodes: int
     learning_rate: float
-    gamme: float
+    gamma: float
     epsilon: float
     map_size: int
     seed: int
@@ -27,7 +27,7 @@ class Params(NamedTuple):
 params = Params(
     total_episodes=2000,
     learning_rate=0.8,
-    gamme=0.95,
+    gamma=0.95,
     epsilon=0.1,
     map_size=5,
     seed=123,
@@ -63,11 +63,13 @@ class Qlearning:
     def update(self, state, action, reward, new_state):
         #bellman equation intution
         # new_Q = old_Q + learning_rate × (reward + gamma × max(next_state's Q-values) − old_Q)
-        ddelta = (
+        delta = (
         reward
         + self.gamma * np.max(self.qtable[new_state, :])
         - self.qtable[state, action]
         )
+        q_update = self.qtable[state, action] + self.learning_rate * delta
+        return q_update
 
     def reset_qtable(self):
         self.qtable = np.zeros((self.state_size, self.action_size))
@@ -88,14 +90,57 @@ class EpsilonGreedy:
     
 
 learner = Qlearning(
-    params.learning_rate, params.gamme, params.state_size, params.action_size
+    params.learning_rate, params.gamma, params.state_size, params.action_size
 )
 explorer = EpsilonGreedy(params.epsilon)
 
-state, info = env.reset(seed=params.seed)
-action = explorer.choose_action(env.action_space, state, learner.qtable)
-next_state, reward, terminated, truncated, info = env.step(action)
-learner.update(state, action, reward, next_state)
+def run():
+    rewards = np.zeros((params.total_episodes, params.n_runs))
+    steps = np.zeros((params.total_episodes, params.n_runs))
+    episodes = np.arange(params.total_episodes)
+    qtables = np.zeros((params.n_runs, params.state_size, params.action_size))
+    all_states = []
+    all_actions = []
 
-print("Updated Q-Table row for state:", state)
-print(learner.qtable[state, :])
+    for run in range(params.n_runs):
+        learner.reset_qtable()
+
+        for episode in tqdm(episodes, desc=f"Run {run}/{params.n_runs} - Episdoes", leave=False):
+            state = env.reset(seed=params.seed)[0]
+            step = 0
+            done = False
+            total_rewards = 0
+
+            while not done:
+                action = explorer.choose_action(
+                    action_space=env.action_space, state=state, qtable=learner.qtable
+                )
+
+                all_states.append(state)
+                all_actions.append(action)
+
+                new_state, reward, terminated, truncated, info = env.step(action)
+                done = terminated or truncated
+                learner.qtable[state, action] = learner.update(
+                    state, action, reward, new_state
+                )
+
+                total_rewards += reward
+                step += 1
+
+                state = new_state
+            rewards[episode, run] = total_rewards
+            steps[episode, run] = step
+        qtables[run, :, :] = learner.qtable
+
+    return rewards, steps, episodes, qtables, all_states, all_actions
+
+result = run()
+rewards, steps, episodes, qtables, all_states, all_actions = result
+
+print("Completed - Results")
+print(f"{params.n_runs} runs × {params.total_episodes} episodes")
+print(f"Avg last 100 episodes: {rewards[-100:].mean():.3f}")
+print(f"Avg steps per episode: {steps.mean():.1f}")
+print(f"Qtable shape: {qtables[-1].shape}")
+print(f"State-Action pairs: {len(set(zip(all_states, all_actions)))}")
